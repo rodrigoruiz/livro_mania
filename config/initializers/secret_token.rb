54461ce994cef07ddfae301bcd4618709b7eed70
Rodrigo Ruiz <rodrigo.ruiz7@gmail.com>
2012-04-03 14:05:56 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LivroMania::Application.config.secret_token = 'fc73fe5c9f6d5628cf3003a365358fc1ca04007284c28fce474861458def597408b24d9f371f4ba5f7b31d27eee73a09f2a9febe711c69848d1d7d8e7edfec7d'
