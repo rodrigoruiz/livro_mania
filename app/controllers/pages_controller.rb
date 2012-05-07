4c0cb0f57e654485431c722604edd694717b93e7
Rodrigo Ruiz <rodrigo.ruiz7@gmail.com>
2012-05-07 19:36:07 -0300

    Modificado o header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def index
     #@my_books = current_user.my_books
     @my_books = []
-    @desired_books = current_user.books
+    @desired_books = current_user.books if current_user
   end
 end
