e4639cd89124ab78f0807a72fdb2f3d1657664f0
Heitor Vieira <halbuquerquevieira@gmail.com>
2012-05-07 23:06:54 -0300

    Agora com o geocode

--- app/models/my_book.rb
+++ app/models/my_book.rb
@@ -4,10 +4,6 @@ class MyBook < ActiveRecord::Base
 
   attr_accessible :user_id, :book_id
   
-  #, :class_name => 'Book'
-  #, :class_name => 'User'
-=======
->>>>>>> 940e0d3c96d2facac288d6e4b37484bf1d157e1c
   belongs_to :book, :class_name => 'Book'
   belongs_to :owner, :class_name => 'User'
 end
