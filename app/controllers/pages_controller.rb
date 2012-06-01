46f51d4bdc9a5bc16c6a26273fda58e5fcae7f3e
Rodrigo Ruiz <rodrigo.ruiz7@gmail.com>
2012-06-01 14:58:51 -0300

    Missing book notification email delivery system ready (missing add notification button)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   autocomplete :book, :title
+  
   def index
     if current_user
       @my_books = current_user.my_books.sort_by { |my_book| my_book.title }
@@ -12,41 +13,6 @@ class PagesController < ApplicationController
     end
     
     @book = Book.new
-  end
-  
-  def new_my_book
-    @my_book = MyBook.new
-    @my_book.book = Book.new
-  end
-  
-  def create_my_book
-    title = params[:my_book][:title]
-    params[:my_book].delete(:title)
-    my_book = MyBook.new(params[:my_book])
-    my_book.book = Book.find_by_title(title)
-    my_book.owner = current_user
-    my_book.save
-    
-    redirect_to root_path
-  end
-  
-  def remove_my_book
-    MyBook.find(params[:id]).destroy
-    
-    redirect_to root_path
-  end
-  
-  def add_desired_book
-    book = Book.where(:title => params[:book][:title]).first
-    current_user.books << book if book && !current_user.books.include?(book)
-    
-    redirect_to root_path
-  end
-  
-  def remove_desired_book
-    current_user.books.delete(Book.find(params[:id]))
-    
-    redirect_to root_path
   end
   
   def user_profile
---
+++ app/models/missing_book_notification.rb
@@ -0,0 +1,6 @@
+class MissingBookNotification < ActiveRecord::Base
+  attr_accessible :user, :book
+  
+  belongs_to :user
+  belongs_to :book
+end
